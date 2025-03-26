5b002cfbaa358a6496029c138afd12211910fdb1
Mr.Ender <72108651+thecodemonster-AI@users.noreply.github.com>
2025-03-26 19:47:44 -0400

    Add files via upload

--- common/src/main/java/org/zeith/cloudflared/CloudflaredMod.java
+++ common/src/main/java/org/zeith/cloudflared/CloudflaredMod.java
@@ -7,24 +7,20 @@ import org.zeith.cloudflared.proxy.CommonProxy;
 import java.io.IOException;
 import java.nio.file.Path;
 
-public class CloudflaredMod
-{
-	public static final Logger LOG = LogManager.getLogger("CloudflaredAPI/Mod");
-	public static final String MOD_ID = "cloudflared";
-	
-	public static CommonProxy PROXY;
-	
-	public static void init(Path configFile)
-	{
-		PROXY.setup();
-		
-		try
-		{
-			CloudflaredConfig.configFile = configFile;
-			CloudflaredConfig.load();
-		} catch(IOException e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
+public class CloudflaredMod {
+    public static final Logger LOG = LogManager.getLogger("CloudflaredAPI/Mod");
+    public static final String MOD_ID = "cloudflared";
+
+    public static CommonProxy PROXY;
+
+    public static void init(Path configFile) {
+        PROXY.setup();
+
+        try {
+            CloudflaredConfig.configFile = configFile;
+            CloudflaredConfig.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
