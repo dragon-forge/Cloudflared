45b58f9e2da1147e4083b84a8d84ff3ede22d641
Mr.Ender <72108651+thecodemonster-AI@users.noreply.github.com>
2025-03-26 19:45:51 -0400

    Add files via upload

--- forge/src/main/java/org/zeith/cloudflared/forge/CloudflaredForge.java
+++ forge/src/main/java/org/zeith/cloudflared/forge/CloudflaredForge.java
@@ -2,7 +2,9 @@ package org.zeith.cloudflared.forge;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.*;
+import net.minecraftforge.event.server.ServerStartedEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
@@ -11,34 +13,29 @@ import net.minecraftforge.fml.loading.FMLPaths;
 import org.zeith.cloudflared.CloudflaredMod;
 
 @Mod(CloudflaredMod.MOD_ID)
-public class CloudflaredForge
-{
-	public CloudflaredForge()
-	{
-		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		
-		MinecraftForge.EVENT_BUS.addListener(this::serverStarting);
-		MinecraftForge.EVENT_BUS.addListener(this::serverStarted);
-		MinecraftForge.EVENT_BUS.addListener(this::serverStopped);
-		
-		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> CloudflaredForgeClient::setup);
-		DistExecutor.safeRunWhenOn(Dist.DEDICATED_SERVER, () -> CloudflaredForgeServer::setup);
-		
-		CloudflaredMod.init(FMLPaths.CONFIGDIR.get().resolve("cloudflared.cfg"));
-	}
-	
-	private void serverStarting(ServerStartingEvent e)
-	{
-		CloudflaredMod.PROXY.serverStarting(e.getServer());
-	}
-	
-	private void serverStarted(ServerStartedEvent e)
-	{
-		CloudflaredMod.PROXY.serverStarted(e.getServer());
-	}
-	
-	private void serverStopped(ServerStoppingEvent e)
-	{
-		CloudflaredMod.PROXY.serverStop();
-	}
+public class CloudflaredForge {
+    public CloudflaredForge() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.addListener(this::serverStarting);
+        MinecraftForge.EVENT_BUS.addListener(this::serverStarted);
+        MinecraftForge.EVENT_BUS.addListener(this::serverStopped);
+
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> CloudflaredForgeClient::setup);
+        DistExecutor.safeRunWhenOn(Dist.DEDICATED_SERVER, () -> CloudflaredForgeServer::setup);
+
+        CloudflaredMod.init(FMLPaths.CONFIGDIR.get().resolve("cloudflared.cfg"));
+    }
+
+    private void serverStarting(ServerStartingEvent e) {
+        CloudflaredMod.PROXY.serverStarting(e.getServer());
+    }
+
+    private void serverStarted(ServerStartedEvent e) {
+        CloudflaredMod.PROXY.serverStarted(e.getServer());
+    }
+
+    private void serverStopped(ServerStoppingEvent e) {
+        CloudflaredMod.PROXY.serverStop();
+    }
 }
--- forge/src/main/java/org/zeith/cloudflared/forge/CloudflaredForgeClient.java
+++ forge/src/main/java/org/zeith/cloudflared/forge/CloudflaredForgeClient.java
@@ -6,19 +6,16 @@ import org.zeith.cloudflared.CloudflaredMod;
 import org.zeith.cloudflared.architectury.IMessageConsumer;
 import org.zeith.cloudflared.proxy.ClientProxy;
 
-public class CloudflaredForgeClient
-{
-	private final IMessageConsumer.ForClient messages = new IMessageConsumer.ForClient();
-	
-	static void setup()
-	{
-		CloudflaredForgeClient client = new CloudflaredForgeClient();
-		CloudflaredMod.PROXY = new ClientProxy(client.messages);
-		MinecraftForge.EVENT_BUS.addListener(client::tickEvent);
-	}
-	
-	private void tickEvent(TickEvent.ClientTickEvent e)
-	{
-		messages.clientTick();
-	}
+public class CloudflaredForgeClient {
+    private final IMessageConsumer.ForClient messages = new IMessageConsumer.ForClient();
+
+    static void setup() {
+        CloudflaredForgeClient client = new CloudflaredForgeClient();
+        CloudflaredMod.PROXY = new ClientProxy(client.messages);
+        MinecraftForge.EVENT_BUS.addListener(client::tickEvent);
+    }
+
+    private void tickEvent(TickEvent.ClientTickEvent e) {
+        messages.clientTick();
+    }
 }
