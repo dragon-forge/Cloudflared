db75e604868cb8f05b61d5e164d3fefc89eb1281
Mr.Ender <72108651+thecodemonster-AI@users.noreply.github.com>
2025-03-26 19:49:46 -0400

    Add files via upload

--- common/src/main/java/org/zeith/cloudflared/core/util/CloudflaredVersion.java
+++ common/src/main/java/org/zeith/cloudflared/core/util/CloudflaredVersion.java
@@ -5,22 +5,19 @@ import lombok.EqualsAndHashCode;
 import java.util.regex.Pattern;
 
 @EqualsAndHashCode
-public class CloudflaredVersion
-{
-	public static final Pattern CFD_VER_REGEX = Pattern.compile("(?<version>\\d\\S+).+\\s(?<built>\\d[^)]+)");
-	
-	public final String version;
-	public final String buildTime;
-	
-	public CloudflaredVersion(String version, String buildTime)
-	{
-		this.version = version;
-		this.buildTime = buildTime;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return String.format("cloudflared version %s (built %s)", version, buildTime);
-	}
+public class CloudflaredVersion {
+    public static final Pattern CFD_VER_REGEX = Pattern.compile("(?<version>\\d\\S+).+\\s(?<built>\\d[^)]+)");
+
+    public final String version;
+    public final String buildTime;
+
+    public CloudflaredVersion(String version, String buildTime) {
+        this.version = version;
+        this.buildTime = buildTime;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("cloudflared version %s (built %s)", version, buildTime);
+    }
 }
--- common/src/main/java/org/zeith/cloudflared/core/util/UploadProgress.java
+++ common/src/main/java/org/zeith/cloudflared/core/util/UploadProgress.java
@@ -3,23 +3,18 @@ package org.zeith.cloudflared.core.util;
 /**
  * Callback interface for reporting upload progress for a request.
  */
-public interface UploadProgress
-{
-	/**
-	 * Callback invoked as data is uploaded by the request.
-	 *
-	 * @param uploaded
-	 * 		The number of bytes already uploaded
-	 * @param total
-	 * 		The total number of bytes that will be uploaded or -1 if
-	 * 		the length is unknown.
-	 */
-	void onUpload(long uploaded, long total);
-	
-	UploadProgress DEFAULT = new UploadProgress()
-	{
-		public void onUpload(long uploaded, long total)
-		{
-		}
-	};
+public interface UploadProgress {
+    UploadProgress DEFAULT = new UploadProgress() {
+        public void onUpload(long uploaded, long total) {
+        }
+    };
+
+    /**
+     * Callback invoked as data is uploaded by the request.
+     *
+     * @param uploaded The number of bytes already uploaded
+     * @param total    The total number of bytes that will be uploaded or -1 if
+     *                 the length is unknown.
+     */
+    void onUpload(long uploaded, long total);
 }
